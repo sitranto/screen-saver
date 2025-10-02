4926eb33cf97c0c28a1470fa2a04d34b2d78e3d9
sitranto <89784211+sitranto@users.noreply.github.com>
2025-10-03 01:51:01 +0300

    Generating, moving and painting snowflakes

---
+++ screen-saver/Snowflake.cs
@@ -0,0 +1,9 @@
+﻿namespace screen_saver
+{
+    internal class Snowflake
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int Size { get; set; }
+    }
+}
